e23e80ef1eb729cf8b68315774d0274451fbd283
ebisLab <ebaiden@techie.com>
2020-01-30 15:53:15 -0500

    testing passing props

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,17 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import {info} from './data'
 
+const Post = (props) =>{
+  return(<div>
+    <h2>I'm sure you are</h2>
+  </div>)
+}
 
+const Another = (props) =>{
+  console.log('props in another=>', props)
+  console.log('name: ',props.data.name)
+  return(
+    <div style={{color: 'red'}}>
+      <h2>Inside Another component</h2>
+{props.data.map(item=>(<Post item={item}/>))}
+    </div>
+  )
+}
 
 function App(props) {
-  const [data, setData] = useState(info);
+  const [data, setData] = useState( info
+//     {
+//     id:0,
+// name: 'Ebi D Navigator',
+// }
+);
 
   console.log(data, '<==props')
   return (
     <div className="App">
       <h2>Hello from inside App component</h2>
-      {/* <Team data={data} /> */}
-{/* {props.data.map((inf)=><div>Hello World</div>)} */}
+
+
+      <Another data={data}/>
+      {data.map(item=>(<Post item={item}/>))}
     
     </div>
   );
